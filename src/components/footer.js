bc3dd5538336d1432320ea7ff05c779c78d85e02
cisco <sovisrushain@gmail.com>
2020-11-22 18:38:12 +0530

    Finish basic UI.

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Footer = () => {
     return ( 
-        <h1>Footer</h1>
+        <div className='row row-6 mt-5'>
+            <p className='mx-auto'>2020 Cisco&copy;</p>
+        </div>
      );
 }
  
