7612c85e4b01fc696ea4b53812bc13ca95ad7ffa
cisco <sovisrushain@gmail.com>
2020-11-22 13:02:07 +0530

    Finish Header component.

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import Logo from '../images/planet-earth.png';
 
 const Header = () => {
-    return ( 
-        <h1>Header</h1>
+    return (
+        <React.Fragment>
+            <div className='row row-1'>
+                <img className='img mx-auto mt-5' src={Logo}/>
+            </div>
+            <div className='row row-2'>
+                <h1 className='heading mx-auto'>Geography 101</h1>
+            </div>
+            <div className='row row-3'>
+                <p className='mx-auto'>Test Your Geography Skill</p>
+            </div>
+        </React.Fragment> 
      );
 }
  
